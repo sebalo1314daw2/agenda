01496df161f792fe439f7fd64e26f795a20b6365
sebalo1314daw2 <sebalo1314daw2@gmail.com>
2014-06-21 19:04:12 +0200

    Agenda de butxaca 2.0.0
    - Crear la interfaz del registro
    - crear el layout fijo para movil

---
+++ js/control/generalFunctions.js
@@ -0,0 +1,62 @@
+$("document").ready(function(){atTheStartOfPage();});
+/**
+ * generateLogo()
+ * @description Procedure that is intended to generate the logo of the web.
+ * @author Sergio Baena López
+ * @version 1.0
+ */
+function generateLogo()
+{
+    var logo = '<a href="login.html" title="Anar a la p&agrave;gina d\'inici">';
+    logo +=         '<img src="../img/logo.png" alt="Logotip" />';      
+    logo +=         'Agenda de butxaca';
+    logo +=    '</a>';                 
+    $("#logo").append
+    (
+            $(logo)
+    );
+}
+/**
+ * generateMenu()
+ * @description Procedure that is intended to generate the menu of the web.
+ * @author Sergio Baena López
+ * @version 1.0
+ */
+function generateMenu()
+{
+    var menu =  "<ul>";
+    menu     +=     '<li class="currentOptionMenu" >';
+    menu     +=         '<a class="currentOptionMenu" href="javascript:void(0)">Acc&eacute;s</a>';
+    menu     +=     '</li>';
+    menu     +=     '<li>';
+    menu     +=         '<a href="javascript:void(0)">Registre</a>';
+    menu     +=     '</li>';
+    menu     +=     '<li>';   
+    menu     +=         '<a href="javascript:void(0)">Sobre la web</a>';  
+    menu     +=     '</li>';  
+    menu     +=     '<li>';                     
+    menu     +=         '<a href="javascript:void(0)">Contacte</a>';                       
+    menu     +=     '</li>';                          
+    menu     += '</ul>';                   
+    $("#menu").append
+    (
+            $(menu)
+    );              
+}
+/**
+ * generateFooter()
+ * @description Procedure that is intended to generate the footer of the web.
+ * @author Sergio Baena López
+ * @version 1.0
+ */
+function generateFooter()
+{
+    var footer =    '<p>';
+    footer     +=           'Servei gratu&iuml;t que ofereix el Sergio Baena L&oacute;pez per a tothom ';
+    footer     +=           'que hi vulgui.';
+    footer     +=   '</p>';                 
+    $("#footer").append
+    (
+            $(footer)
+    );              
+}
